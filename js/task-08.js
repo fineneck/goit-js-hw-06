d533f83325b5ce33bccc90bfeeb6d410fcd424f3
OleksiiMikhieiev <babybong0993@gmail.com>
2023-02-22 11:28:03 +0200

    t-08-09

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit (event) {
+  event.preventDefault();
+
+  const { elements: { email, password }, } = event.currentTarget;
+
+  if (email.value === '' || password.value === '') {
+    return alert('Please fill in all the fields')
+  };
+  
+  const userRefs = { email: email.value, password: password.value };
+
+  console.log(userRefs);
+  event.currentTarget.reset(); 
+};
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,19 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const refs = {
+  body: document.querySelector('body'),
+  spanColor: document.querySelector('.color'),
+  btnColor: document.querySelector('.change-color'),
+};
+
+refs.btnColor.addEventListener('click', onBtnClick);
+
+function onBtnClick() {
+  const changeColor = getRandomHexColor();
+  refs.spanColor.textContent = changeColor;
+  refs.body.style.backgroundColor = changeColor;
 }
